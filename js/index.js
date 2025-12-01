d1ff778ed8614485821b54f648f5d428f617d420
David Bouhaben <davidbouhaben@yahoo.co.id>
2025-12-01 22:44:50 +0700

    button click gtone page

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 // Rock Paper Scissors Game Logic
 
+const welcomePage = document.getElementById("welcome-page");
+const playButton = document.getElementById("start-game-btn");
+
+playButton.addEventListener("click", (e) => {
+  welcomePage.classList.add("welcome-page-hidden");
+});
