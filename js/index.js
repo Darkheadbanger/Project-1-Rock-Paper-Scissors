8e80d417841554670879a62eb5b3af6ac52bd739
David Bouhaben <davidbouhaben@yahoo.co.id>
2025-12-04 14:29:25 +0700

    Refactor code structure for improved readability and maintainability

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@
 
 const welcomePage = document.getElementById("welcome-page");
 const playButton = document.getElementById("start-game-btn");
+const loadingPage = document.getElementById("loading-page");
 
 playButton.addEventListener("click", (e) => {
   welcomePage.classList.add("welcome-page-hidden");
+  welcomePage.classList.add("loading-page-show");
 });
